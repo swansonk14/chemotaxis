3632141fdeee76d5fc62bdff364b795c955ea0ac
Kyle Swanson <swansonk.14@gmail.com>
2021-07-27 12:13:43 +0100

    Plot multiple cells swimming

--- plot_cell_simulation.py
+++ plot_cell_simulation.py
@@ -4,12 +4,11 @@ from pathlib import Path
 from matplotlib.collections import LineCollection
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from tap import Tap
 
 
 # Header for RapidCell output file
-NAMES = ['t', 'x', 'y', 'orientation', 'CheA-P', 'CheY-P', 'methylation', 'CCW_bias']
+COLUMNS = ['t', 'x', 'y', 'orientation', 'CheA-P', 'CheY-P', 'methylation', 'CCW_bias']
 
 
 class Args(Tap):
@@ -19,21 +18,29 @@ class Args(Tap):
 def plot_cell_simulation(args: Args) -> None:
     """Plots the movement of cells in a RapidCell simulation."""
     # Load data
-    data = pd.read_csv(args.data_path, sep='\t', names=NAMES, index_col=False)
-
-    # Extract line segments from cell movement
-    points = np.array([data['x'], data['y']]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    data = np.loadtxt(args.data_path)
+    t = data[:, 0]  # (num_time_points,)
+    X = data[:, 1::7].T  # (num_cells, num_time_points)
+    Y = data[:, 2::7].T  # (num_cells, num_time_points)
 
     # Plot cell movements
     fig, ax = plt.subplots()
-    lc = LineCollection(segments, cmap='viridis')
-    lc.set_array(data['t'])
-    lc.set_linewidth(2)
-    line = ax.add_collection(lc)
+
+    # Iterate over individual cells
+    for x, y in zip(X, Y):
+        # Extract line segments from cell movement
+        points = np.array([x, y]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # Plot line segments
+        lc = LineCollection(segments, cmap='viridis')
+        lc.set_array(t)
+        lc.set_linewidth(2)
+        line = ax.add_collection(lc)
+
     fig.colorbar(line, ax=ax)
-    ax.set_xlim(data['x'].min(), data['x'].max())
-    ax.set_ylim(data['y'].min(), data['y'].max())
+    ax.set_xlim(X.min(), X.max())
+    ax.set_ylim(Y.min(), Y.max())
     plt.show()
 
 
